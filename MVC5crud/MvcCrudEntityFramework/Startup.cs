d2c41fa2a3ff7070f9538eeb060f94c0f5ebd3e2
unknown <haile.nahu18@gmail.com>
2020-01-19 11:14:02 -0900

    mvc5 crud app

---
+++ MVC5crud/MvcCrudEntityFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcCrudEntityFramework.Startup))]
+namespace MvcCrudEntityFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
